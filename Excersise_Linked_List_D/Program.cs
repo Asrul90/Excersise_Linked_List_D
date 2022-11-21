c21e6ce6084b023813d4c759d0eb69ab2e4cb6c6
M S I <M S I@MSI>
2022-11-21 15:43:15 +0700

    add clas node

--- Excersise_Linked_List_D/Program.cs
+++ Excersise_Linked_List_D/Program.cs
@@ -8,13 +8,12 @@ namespace Excersise_Linked_List_D
 {
     class Node
     {
-        /* cretes node for the circular nexted list*/
+        /* cretes node for the circular nexted lisst*/
         public int rollNumber;
         public string name;
         public Node next;
     }
 
-    
     internal class Program
     {
         static void Main(string[] args)
